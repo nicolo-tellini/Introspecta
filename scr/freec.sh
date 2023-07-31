fa11e919deec4acf2fac9e2ac16f4657233facd9
nt <nicolo.tellini@unice.fr>
2023-07-31 19:28:50 +0200

    Update freec.sh

--- scr/freec.sh
+++ scr/freec.sh
@@ -1,62 +1,61 @@
 #!/bin/bash
 
-BaseDir=$1
+baseDir=$1
+nThreads=$2
 
-TemplateFile=$BaseDir"/scr/Control-FREEC.Config.Template.txt"
-Path2CFlib=$BaseDir"/scr/"
-
-Nthreads=4
-mapDir=$BaseDir"/map"
-DataExt=".rmd.bam"
+tfile=$baseDir"/scr/Control-FREEC.Config.Template.txt"
+p2lib=$baseDir"/scr/"
+mapDir=$baseDir"/map"
+DataExt=".bam"
 
 # retrieve read len from seq/reand.len for plotting
 
-for indS in $(cat $BaseDir/seq/read.len | cut -f1)
+for ind in $(cat $baseDir/seq/read.len | cut -f1 | sed 's+_1++g' | sed 's+_2++g' | sort -u)
  do
 
  ploidy=2
- readlen=$(grep -w $indS $BaseDir/seq/read.len | sort | uniq | cut -f2)
+ readlen=$( grep $ind $baseDir/seq/read.len | cut -f2 | sort -u)
 
-if [[ ! -s $TemplateFile ]]; then
+if [[ ! -s $tfile ]]; then
   echo "Missing template file"
   exit 1
 fi
 
 cd $mapDir
 
-for indB in $(ls $indS"."*${DataExt})
+for indB in $(ls $ind"."*${DataExt})
 do
- if (( i % Nthreads == 0 )); then
+ if (( i % nThreads == 0 )); then
   wait
  fi
  ((i++))
  (
  reference=$(echo $indB | cut -d"." -f2)
- refPath=$(echo $BaseDir"/ref/"$reference".genome.fa")
+ refPath=$(echo $baseDir"/ref/"$reference".genome.fa")
  SampleName=$(echo $indB | cut -d "." -f 1)
 
-  OutDir=$BaseDir"/cnv/results/"$reference/$SampleName
+  OutDir=$baseDir"/cnv/results/"$reference/$SampleName
   if [[ ! -d $OutDir ]]; then
     mkdir -p $OutDir
   fi
 
-  Config=$BaseDir"/cnv/config/"$reference"/"$SampleName
+  Config=$baseDir"/cnv/config/"$reference"/"$SampleName
   if [[ ! -d $Config ]]; then
     mkdir -p $Config
   fi
 
   # crea il file config_GC.txt specifico per il campione/reference
-  sed "s|STRINGchrLenFile|$BaseDir/cnv/GCdata/$reference/LenChr.txt|" < $TemplateFile | sed "s|STRINGchrFiles|$BaseDir/cnv/GCdata/$reference/Chrref|" | sed "s|STRINGmateFile|$BaseDir/map/$SampleName.$reference.srt.rmd.bam|" | sed "s|STRINGoutputDir|$OutDir|" | sed "s|NumPloidy|$ploidy|" | sed "s|STRINGgemMappabilityFile|$BaseDir/cnv/mappability/$reference/$readlen/$reference.mappability|" | sed -e "s|Sambamba|$BaseDir/scr/sambamba|" > $Config"/CF."$SampleName".Input.GC.txt"
+  sed "s|STRINGchrLenFile|$baseDir/cnv/GCdata/$reference/LenChr.txt|" < $tfile | sed "s|STRINGchrFiles|$baseDir/cnv/GCdata/$reference/Chrref|" | sed "s|STRINGmateFile|$baseDir/map/$SampleName.$reference.bam|" | sed "s|STRINGoutputDir|$OutDir|" | sed "s|NumPloidy|$ploidy|" | sed "s|STRINGgemMappabilityFile|$baseDir/cnv/mappability/$reference/$readlen/$reference.mappability|" | sed -e "s|Sambamba|$baseDir/scr/sambamba|" > $Config"/CF."$SampleName".Input.GC.txt"
 
   # running Control-FREEC
   freec -conf $Config"/CF."$SampleName".Input.GC.txt" &> $OutDir"/"$SampleName".log"
 
   # calculate significance
-  cat $BaseDir/scr/assess_significance.R | R --slave --args $OutDir"/"$SampleName"."$reference".srt.rmd.bam_CNVs" $OutDir"/"$SampleName"."$reference".srt.rmd.bam_ratio.txt"
+  Rscript $baseDir/scr/assess_significance.R $OutDir"/"$SampleName"."$reference".bam_ratio.txt" $OutDir"/"$SampleName"."$reference".bam_CNVs"
 
-  AllChr=$(cut -f2 $BaseDir"/cnv/GCdata/"$reference"/LenChr.txt" | sed 's|chr||')
+  AllChr=$(cut -f2 $baseDir"/cnv/GCdata/"$reference"/LenChr.txt" | sed 's|chr||')
   Nchrom=$(echo ${AllChr} | wc -w)
-  cat $Path2CFlib"/makeplotcnv.R" | R --slave --args $ploidy $OutDir"/"$SampleName"."$reference".srt.rmd.bam_ratio.txt" $Nchrom $AllChr
+  cat $p2lib"/makeplotcnv.R" | R --slave --args $ploidy $OutDir"/"$SampleName"."$reference".bam_ratio.txt" $Nchrom $AllChr
   ) &
  done
 done
